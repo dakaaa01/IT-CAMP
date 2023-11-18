8dbcae25964f718b2f8c76cdd6e5d4c88952f11a
dakaaa01 <147754198+dakaaa01@users.noreply.github.com>
2023-11-18 20:14:08 +0100

    first commit

---
+++ vezbanje.js
@@ -0,0 +1,41 @@
+// 10.4. Саставити програм који ће учитати низ од n елемената и исписати их оним редоследом којим
+// су учитани.
+
+// let pitanje=parseInt(prompt('Broj ponavljanja koji zelite je:'))
+// let niz=[];
+
+// for(i=0; i<pitanje; i++){
+//     let elementiNiza=parseInt(prompt('Unesite zeljene elemente'))
+//     niz.push(elementiNiza)
+// }
+// console.log(niz);
+
+// 10.5. Саставити програм који ће учитати низ од n елемената и исписати их обрнутим редоследом
+
+// let pitanje=parseInt(prompt('Broj ponavljanja koji zelite je:'))
+// let niz=[];
+
+// for(i=0; i<pitanje; i++){
+//     let elementiNiza=parseInt(prompt('Unesite zeljene elemente'))
+//     niz.push(elementiNiza)
+// }
+// console.log(niz.reverse());
+
+// 10.7. Саставити програм за израчунавање и исписивање аритметичке средине задатог низа (дужине
+//     n) целих бројева.
+
+// let pitanje=parseInt(prompt('Broj ponavljanja koji zelite je:'))
+//  let niz=[];
+
+// function artSredina (n){
+//     let sredina=0;
+//     for(let i=0; i<niz.length; i++){
+//     sredina+=niz[i]
+//     }
+//     return sredina
+//     console.log(artSredina/i);
+//     let elementiNiza=parseInt(prompt('Unesite zeljene elemente'))
+// }
+// let elementiNiza=parseInt(prompt('Unesite zeljene elemente'))
+// console.log(artSredina/i);
+
